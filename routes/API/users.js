4d0e757700c3b91f00463345dde46089718c0b7f
Mikko McMenamin <mikko.mcmenamin@gmail.com>
2019-07-30 22:38:20 +0300

    Add payload and JWT

--- routes/API/users.js
+++ routes/API/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {check, validationResult} = require("express-validator");
 
 const User = require('../../models/User');
@@ -36,7 +38,7 @@ router.post(
             let user = await User.findOne({ email});
 
             if(user) {
-                res.status(400).json({errors: [{ msg: 'User already exists'}] });
+                return res.status(400).json({errors: [{ msg: 'User already exists'}] });
             }
 
             const avatar = gravatar.url(email, {
@@ -59,9 +61,23 @@ router.post(
 
             await user.save();
 
-            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id,
+                }
+            };
 
-            res.send("User registered");
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {
+                    expiresIn: 36000
+                },
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({token})
+                });
+
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server error');
