b6c48b1e60c8c42fc6f588cff798b4d4f7a6583a
Mikko McMenamin <mikko.mcmenamin@gmail.com>
2019-08-29 22:57:28 +0300

    Login and Register functionality

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -14,14 +15,14 @@ const Register = () => {
   const onChange = e =>
     setFormData({...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if(password !== password2 ){
       console.log("Passwords do not match");
     }else{
-      console.log(formData);
+      console.log("SUCCESS");
     }
-  }
+  };
 
   return (
     <Fragment>
@@ -63,7 +64,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <Link to="login.html">Sign In</Link>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
