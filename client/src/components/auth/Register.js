fcbb6a9f434e1eabb3a72dfb451588698b0a6c9d
Mikko McMenamin <mikko.mcmenamin@intopalo.com>
2019-08-30 15:01:14 +0300

    Set up redux and alert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,9 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import {Link} from "react-router-dom";
-import axios from "axios";
+import { setAlert }  from "../../actions/alert";
 
-const Register = () => {
+const Register = (props) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,7 +19,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if(password !== password2 ){
-      console.log("Passwords do not match");
+      props.setAlert("Passwords do not match", "danger");
     }else{
       console.log("SUCCESS");
     }
@@ -70,4 +71,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default connect(null, { setAlert })(Register);
