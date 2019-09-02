d699844b369e066110a48bce289ca782b32fc4ca
Mikko McMenamin <mikko.mcmenamin@intopalo.com>
2019-09-02 13:16:23 +0300

    Dispatch alert message with Redux

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,5 +1,5 @@
 import uuid from "uuid";
-import {SET_ALERT, REMOTE_ALERT} from "./types";
+import {SET_ALERT, REMOVE_ALERT} from "./types";
 
 export const setAlert = (msg, alertType) => dispatch => {
     const id = uuid.v4();
@@ -7,4 +7,6 @@ export const setAlert = (msg, alertType) => dispatch => {
         type: SET_ALERT,
         payload: { msg, alertType, id }
     });
-}
+
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000);
+};
