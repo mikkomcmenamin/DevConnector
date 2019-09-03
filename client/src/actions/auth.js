2248c90bff9a4b20da3c5822926742b5a70283e0
Mikko McMenamin <mikko.mcmenamin@gmail.com>
2019-09-03 16:01:59 +0300

    User login, Logout and Navbar links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,8 +3,33 @@ import { setAlert } from "./alert";
 
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT
 } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+
+// Load User
+export const loadUser = () => async dispatch =>  {
+    if(localStorage.token){
+        setAuthToken(localStorage.token);
+    }
+
+    try{
+        const res = await axios.get("/api/auth");
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        });
+    }catch (e) {
+        dispatch({
+            type: AUTH_ERROR
+        });
+    }
+}
 
 // Register User
 export const register = ({ name, email, password}) => async dispatch => {
@@ -13,8 +38,6 @@ export const register = ({ name, email, password}) => async dispatch => {
             "Content-Type": "application/json"
         }
     };
-
-
 
     const body = JSON.stringify({ name, email, password});
 
@@ -26,6 +49,7 @@ export const register = ({ name, email, password}) => async dispatch => {
             payload: res.data
         });
 
+        dispatch(loadUser());
 
     }catch (err) {
 
@@ -39,5 +63,43 @@ export const register = ({ name, email, password}) => async dispatch => {
         });
 
     }
+};
+
+// Login User
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    const body = JSON.stringify({email, password});
+
+    try{
+        const res = await axios.post("/api/auth", body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+
+        dispatch(loadUser());
+
+    }catch (err) {
+        const errors = err.response.data.errors;
+
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+        }
+        dispatch({
+            type: LOGIN_FAIL
+        });
+
+    }
+};
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
 };
 
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from "react";
-import {Link} from "react-router-dom";
-import axios from "axios";
+import {Link, Redirect} from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -15,8 +17,13 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-    console.log("SUCCESS");
+    login(email, password);
   };
+
+  //Redirect if logged in
+  if(isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <Fragment>
@@ -26,7 +33,7 @@ const Login = () => {
       </p>
       <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)}  />
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required/>
         </div>
         <div className="form-group">
           <input
@@ -36,6 +43,7 @@ const Login = () => {
             value={password}
             onChange={e => onChange(e)}
             minLength="6"
+            required
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Login" />
@@ -47,4 +55,13 @@ const Login = () => {
   );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
